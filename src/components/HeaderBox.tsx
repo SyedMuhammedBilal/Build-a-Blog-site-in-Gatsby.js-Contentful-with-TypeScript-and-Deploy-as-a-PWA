ca37ad1dd82d8022d46d71d35a21626160f73b23
Syed Muhammed Bilal <intergo878@gmail.com>
2020-08-04 16:58:56 +0500

    Add files via upload

--- src/components/HeaderBox.tsx
+++ src/components/HeaderBox.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 import "../styles/HeaderBox.css"
 
 const HeaderBox = () => {
+
+  function clickHandler() {
+    console.log("Increment Clicked");
+  }
+
   return (
     <div className="container">
       <div className="box">
@@ -10,7 +15,7 @@ const HeaderBox = () => {
           <h3>La Casa de Papel</h3>
           <p>Season 05 shooting started, will be out soon on 2021!<br />Season: 01-04 available only on <b>NETFLIX</b></p>
           <div className="btn-cont">
-            <button className="btn">Explore</button>
+            <button onClick={clickHandler} className="btn">Explore</button>
           </div>
         </div>
       </div>
