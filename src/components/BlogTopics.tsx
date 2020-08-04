092e508cf0dad83d6594e75a5b860a026e780132
Syed Muhammed Bilal <intergo878@gmail.com>
2020-08-04 15:05:11 +0500

    Add files via upload

--- src/components/BlogTopics.tsx
+++ src/components/BlogTopics.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby';
+
+import "../styles/BlogTopics.css"
 
 const BlogTopics = () => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulBlogType {
+        edges {
+          node {
+            image {
+              file {
+                url
+              }
+            }
+            title
+          }
+        }
+      }
+    }
+  `)
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="wht-cont">
+        <h2>Whats'up?</h2>
+        <h3>Here New Blogs</h3>
+      </div>
+      <div className="blog-cont">
+        {data.allContentfulBlogType.edges.map((edge: any) => {
+          return (
+            <div className="blog-box">
+              <div className="blog-content">
+                <img src={edge.node.image.file.url} />
+                <h4>{edge.node.title}</h4>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeaderBox from '../components/HeaderBox';
+import BlogTopics from "../components/BlogTopics";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeaderBox />
+    <BlogTopics />
   </Layout>
 )
 
